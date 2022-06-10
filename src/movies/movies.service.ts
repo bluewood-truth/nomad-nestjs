fb8eb9d66d552df0ac52d961229cd18f9adb8ae5
bluewood <bluewood.truth@gmail.com>
2022-06-10 19:36:22 +0900

    update movie service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,11 +10,14 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === parseInt(id));
+    const movie = this.movies.find((movie) => movie.id === parseInt(id));
+    if (!movie) throw new NotFoundException(`Movie not found with id: ${id}`);
+    return movie;
   }
 
   deleteOne(id: string): boolean {
-    this.movies = this.movies.filter((movie) => movie.id === parseInt(id));
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== parseInt(id));
     return true;
   }
 
@@ -25,5 +28,11 @@ export class MoviesService {
     });
 
     return this.movies[this.movies.length - 1];
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
